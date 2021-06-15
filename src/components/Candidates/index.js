414de3234bbefc69f5bd6f094cf04b28e51d576a
Sam Pita <samanthakpita@gmail.com>
2021-06-15 16:40:03 -0500

    basic setup + install

---
+++ src/components/Candidates/index.js
@@ -0,0 +1,13 @@
+import { Typography } from '@material-ui/core'
+import React, { useEffect, useState } from 'react'
+
+const Candidates = () => {
+
+    return (
+
+        <Typography>Candidates page</Typography>
+
+    )
+}
+
+export default Candidates
---
+++ src/pages/Home.js
@@ -0,0 +1,12 @@
+import Candidates from "components/Candidates"
+import React from "react"
+
+const Home = () => {
+  return (
+    <>
+      <Candidates />
+    </>
+  )
+}
+
+export default Home
