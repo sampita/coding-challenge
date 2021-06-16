0ba62f250273acc6d2fd053bb4da8e6d85a44ec8
Sam Pita <samanthakpita@gmail.com>
2021-06-15 19:56:05 -0500

    fix bug when open drawer

--- src/components/Candidates/CandidatesTable/CandidatesTableRow.js
+++ src/components/Candidates/CandidatesTable/CandidatesTableRow.js
@@ -1,32 +1,22 @@
 import React, { useState, useEffect } from "react";
 
 import {
+  Box,
   Checkbox,
   Collapse,
   IconButton,
-  Modal,
+  Table,
   TableCell,
-  TableContainer,
-  TableHead,
   TableRow,
-  Typography,
+  TableBody,
 } from "@material-ui/core";
-import { makeStyles } from "@material-ui/core/styles";
 import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
 import RemoveOutlinedIcon from "@material-ui/icons/RemoveOutlined";
 import ArrowForwardIosIcon from "@material-ui/icons/ArrowForwardIos";
 import moment from "moment";
-import CandidatesModal from "../../Applications/ApplicationModal";
-
-const useStyles = makeStyles(() => ({
-  subTable: {
-      width: '100%',
-  },
-}));
+import ApplicationModal from "../../Applications/ApplicationModal";
 
 const CandidatesTableRow = ({ candidate, selectAll }) => {
-  const classes = useStyles();
-
   const [expanded, setExpanded] = useState(false);
   const [selected, setSelected] = useState(false);
   const [openModal, setOpenModal] = useState(false);
@@ -36,57 +26,69 @@ const CandidatesTableRow = ({ candidate, selectAll }) => {
   }, [selected]);
 
   const handleClose = () => {
-    setOpenModal(false)
-  }
+    setOpenModal(false);
+  };
 
   return (
     <>
-        <TableRow key={candidate?.id}>
-          <TableCell>
-            <Checkbox checked={selectAll ? true : selected} />
-          </TableCell>
-          <TableCell>{candidate?.name}</TableCell>
-          <TableCell>
-            {/* assuming applications array is ordered from most current descending (if not would have to add some ordering either to BE or FE) */}
-            {candidate?.applications[0].new_status.label}
-          </TableCell>
-          <TableCell>{candidate?.applications?.length}</TableCell>
-          <TableCell padding="none" style={{ fontWeight: "bold" }} align="left">
-            {moment(candidate?.profile?.updated).fromNow()}
-          </TableCell>
-          <TableCell padding="none" style={{ fontWeight: "bold" }} align="left">
-            <IconButton onClick={() => setExpanded(!expanded)}>
-              {expanded ? (
-                <RemoveOutlinedIcon />
-              ) : (
-                <AddCircleOutlineOutlinedIcon />
-              )}
-            </IconButton>
-          </TableCell>
-        </TableRow>
-        {/* would break the code below into its own subtable component for code organization if I had time */}
-      <section>
-        <Collapse in={expanded} timeout="auto" unmountOnExit>
-          {candidate?.applications?.map((application) => {
-            return (
-              <div className={classes.subTable}>
-                <TableRow>
-                  <TableCell>{application.role.title}</TableCell>
-                  <TableCell>{application.new_status.label}</TableCell>
-                  <TableCell>&nbsp;</TableCell>
-                  <TableCell>&nbsp;</TableCell>
-                  <TableCell>
-                    <IconButton onClick={() => setOpenModal(!openModal)}>
-                      <ArrowForwardIosIcon fontSize="small" />
-                    </IconButton>
-                  </TableCell>
-                </TableRow>
-                <CandidatesModal openModal={openModal} handleClose={handleClose} application={application} candidate={candidate}/>
-              </div>
-            );
-          })}
-        </Collapse>
-      </section>
+      <TableRow key={candidate?.id}>
+        <TableCell>
+          <Checkbox checked={selectAll ? true : selected} />
+        </TableCell>
+        <TableCell>{candidate?.name}</TableCell>
+        <TableCell>
+          {/* assuming applications array is ordered from most current descending (if not would have to add some ordering either to BE or FE) */}
+          {candidate?.applications[0].new_status.label}
+        </TableCell>
+        <TableCell>{candidate?.applications?.length}</TableCell>
+        <TableCell padding="none" style={{ fontWeight: "bold" }} align="left">
+          {moment(candidate?.profile?.updated).fromNow()}
+        </TableCell>
+        <TableCell padding="none" style={{ fontWeight: "bold" }} align="left">
+          <IconButton onClick={() => setExpanded(!expanded)}>
+            {expanded ? (
+              <RemoveOutlinedIcon />
+            ) : (
+              <AddCircleOutlineOutlinedIcon />
+            )}
+          </IconButton>
+        </TableCell>
+      </TableRow>
+      {/* would break the code below into its own subtable component for code organization if I had time */}
+      <TableRow>
+        <TableCell style={{ paddingBottom: 0, paddingTop: 0 }} colSpan={6}>
+          <Collapse in={expanded} timeout="auto" unmountOnExit>
+            {candidate?.applications?.map((application) => {
+              return (
+                <Box>
+                  <Table>
+                    <TableBody>
+                        <TableRow>
+                        <TableCell>{application.role.title}</TableCell>
+                        <TableCell>{application.new_status.label}</TableCell>
+                        <TableCell>&nbsp;</TableCell>
+                        <TableCell>&nbsp;</TableCell>
+                        <TableCell>&nbsp;</TableCell>
+                        <TableCell align='right'>
+                          <IconButton onClick={() => setOpenModal(!openModal)}>
+                            <ArrowForwardIosIcon fontSize="small" />
+                          </IconButton>
+                        </TableCell>
+                      <ApplicationModal
+                        openModal={openModal}
+                        handleClose={handleClose}
+                        application={application}
+                        candidate={candidate}
+                      />
+                      </TableRow>
+                    </TableBody>
+                  </Table>
+                </Box>
+              );
+            })}
+          </Collapse>
+        </TableCell>
+      </TableRow>
     </>
   );
 };
